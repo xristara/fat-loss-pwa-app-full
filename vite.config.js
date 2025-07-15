0a85db928bfbada9a45f1ee694588af285e1204e
xristara <thegreatreef@yahoo.fr>
2025-07-15 20:16:21 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@src': path.resolve(__dirname, 'src')
+      '@': path.resolve(__dirname, './src'),
     },
   },
-  publicDir: 'public',
-});
+  publicDir: 'public'
+})
