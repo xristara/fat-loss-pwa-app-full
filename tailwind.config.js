71973abde5ac417f9da14ad97b38257c5e3d7fc6
xristara <thegreatreef@yahoo.fr>
2025-07-15 22:28:35 +0200

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
     extend: {},
   },
